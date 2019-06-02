9747c9230b4035fba9b82b18c099e247e6cd7623
Mischa Thullner <mischa@b302.nl>
2019-06-02 17:55:07 +0200

    Setup

---
+++ src/main/java/nl/han/ica/oopd/pacman/Pacman.java
@@ -0,0 +1,21 @@
+package nl.han.ica.oopd.pacman;
+
+import nl.han.ica.oopg.engine.GameEngine;
+import processing.core.PApplet;
+
+public class Pacman extends GameEngine {
+
+    public static void main(String[] args) {
+        String[] processingArgs = {"nl.han.ica.oopd.pacman.Pacman"};
+    }
+
+    @Override
+    public void setupGame(){
+        //
+    }
+
+    @Override
+    public void update(){
+        //
+    }
+}
